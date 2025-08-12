4fb120e3ae5fe26e939e2b15aceefbb27847b874
Chaitanya Malshikare <114339532+chaitanya0802@users.noreply.github.com>
2025-08-12 09:59:23 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,74 @@
+
+public class WebAutomation {
+    /* Use the below declared variable, do not modify anything */
+    public static WebDriver driver;
+    public static String baseUrl = "https://webapps.tekstac.com/RentaCar/";
+
+    /*Do not change*/
+    public WebAutomation() { }
+
+    /*Do not change*/
+    public WebAutomation(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    @BeforeClass
+    public void setupDriver() /*Do not change method signature*/ {
+        // Set the path for the browser driver executable (e.g., geckodriver or chromedriver)
+        // Initialize the WebDriver instance using the selected browser driver
+        // Navigate to the application URL (baseUrl)
+        System.setProperty("webdriver.gecko.driver", "/selenium/geckodriver");
+        FirefoxOptions o = new FirefoxOptions();
+        o.setHeadless(true);
+
+        driver = new FirefoxDriver(o);
+        driver.get(baseUrl);
+    }
+
+    @Test
+    public String verifyResult() /*Do not change method signature*/ {
+        // Enter 'Name' in the respective input field using sendKeys method
+        // Enter 'Phone' number in the respective input field using sendKeys method
+        // Enter 'Address' in the respective input field using sendKeys method
+        // Enter 'Date' in the respective input field using sendKeys method
+        // Select 'Car Type' from the dropdown using the selectByVisibleText method
+        // Enter 'Duration' in the respective input field using sendKeys method
+        // Click the 'Net Banking' button
+        // Click the 'Submit' button
+        // Retrieve and return the confirmation message
+
+        driver.findElement(By.id("name")).sendKeys("chaitanya");
+        driver.findElement(By.id("number")).sendKeys("9587896547");
+        driver.findElement(By.id("address")).sendKeys("fsdfsffsd");
+        driver.findElement(By.id("traveldate")).sendKeys("2024-02-02");
+        Select s = new Select(driver.findElement(By.id("cartype")));
+        s.selectByVisibleText("SUV");
+        driver.findElement(By.id("duration")).sendKeys("2");
+        driver.findElement(By.id("license")).sendKeys("2");
+        driver.findElement(By.id("netbanking")).click();
+        driver.findElement(By.id("submit")).click();
+
+        String res = driver.findElement(By.id("result")).getText();
+        return res;
+    }
+
+    @Test
+    public String verify_PageTitle() /*Do not change method signature*/ {
+        // Retrieve the current page title and return it
+        String pt = driver.getTitle();
+        return pt;
+    }
+
+    // Do not change anything in the main method
+    public static void main(String[] args) {
+        WebAutomation test = new WebAutomation();
+        test.setupDriver();
+        System.out.println("Form Result: " + test.verifyResult());
+        System.out.println("Page Title: " + test.verify_PageTitle());
+    }
+}
+
+===============================================================================
 pipeline {
     agent any
 
@@ -60,3 +131,4 @@ pipeline {
         }
     }
 }
+
